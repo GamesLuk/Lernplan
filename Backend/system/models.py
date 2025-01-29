8ddbcd599516e3c684b792e0f6d80aff8b0d88cc
GamesLuk <lukas@meyer-zu-berstenhorst.de>
2025-01-29 22:34:50 +0100

    Lernzeit Model
    Erstellen einer Table für die Lernzeiten

--- Backend/system/models.py
+++ Backend/system/models.py
@@ -22,3 +22,18 @@ class StudentProfile(models.Model):
 
     def __str__(self):
         return self.name
+    
+class LernzeitProfile(models.Model):
+    lernzeit_ID = models.IntegerField(db_index=True, unique=True)
+    fach = models.CharField(max_length=100,db_index=True)
+    name = models.CharField(max_length=100,db_index=True)
+    art = models.IntegerField(db_index=True)
+    klasse = models.CharField(max_length=10, db_index=True, null=True)
+    stufen = models.JSONField(db_index=True)
+    tag = models.JSONField(db_index=True)
+    stunde = models.JSONField(db_index=True)
+    teacher_school_ID = models.CharField(max_length=100, db_index=True)
+    raum = models.CharField(max_length=10, db_index=True)
+    plätze = models.IntegerField(db_index=True)
+    beschreibung = models.TextField()
+    activ = models.BooleanField(db_index=True)

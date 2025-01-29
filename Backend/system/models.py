e3bbeacbd6ff77f839161134f6137ebf21d79e99
GamesLuk <lukas@meyer-zu-berstenhorst.de>
2025-01-29 20:22:54 +0100

    Umwandlung Binär zu String
    Das Bild muss in Strings gespeichert gewerden.

--- Backend/system/models.py
+++ Backend/system/models.py
@@ -11,12 +11,14 @@ class system(models.Model):
 class StudentProfile(models.Model):
     school_ID = models.IntegerField(db_index=True, unique=True)
     name = models.CharField(max_length=255, db_index=True, unique=True)
+    first_name = models.CharField(max_length=255, db_index=True)
+    last_name = models.CharField(max_length=255, db_index=True)
     email = models.EmailField(unique=True, db_index=True)
-    profile_picture = models.BinaryField(null=True, blank=True, db_index=True)  # Das Bild wird binär gespeichert
-    teams = models.JSONField(default=list, db_index=True)  # Speichert eine Liste der Teams des Benutzers
+    profile_picture = models.TextField(null=True)
+    teams = models.JSONField(default=list, db_index=True, null=True)  # Speichert eine Liste der Teams des Benutzers
     klasse = models.CharField(max_length=1, db_index=True)
     stufe = models.IntegerField(db_index=True)
-    role = models.CharField(max_length=50, db_index=True)
+    role = models.CharField(max_length=255, db_index=True)
 
     def __str__(self):
         return self.name
